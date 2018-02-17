a4803c4d9baa20ff4d0b9daa411a98c390a4225a
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-17 21:06:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,14 @@ def square_array(array)
   array.each {|i| square_array << i ** 2}
   square_array
 end
+
+# using .collect
+# def square_array(array)
+#   array.collect { |i| i ** 2 }
+# end
+
+# using .map
+# def square_array(array)
+#   array.map { |i| i ** 2 }
+# end
+
